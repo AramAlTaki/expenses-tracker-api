548732d89ca5b5d06a4e71dfebc7be9ffb68f791
RaghadKu <rozekuttiny@gmail.com>
2026-01-13 13:05:52 +0300

    Adding Validation

--- src/ExpensesTracker.API/Models/Image.cs
+++ src/ExpensesTracker.API/Models/Image.cs
@@ -1,17 +1,35 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExpensesTracker.API.Data.Models
 {
     public class Image
     {
         public Guid Id { get; set; }
+
+        [Required]
         public Guid TransactionId { get; set; }
+
         [NotMapped]
         public IFormFile File { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "File name cannot exceed 255 characters.")]
         public string FileName { get; set; }
+
+        [Required]
+        [RegularExpression(@"\.(jpg|jpeg|png|webp)$",
+        ErrorMessage = "Only JPG, JPEG, PNG, or WEBP files are allowed.")]
         public string Extension { get; set; }
+
+        [Range(1, 5 * 1024 * 1024,
+        ErrorMessage = "Image size must be less than 5 MB.")]
         public long SizeInBytes { get; set; }
+
+        [Required]
+        [StringLength(500)]
         public string Path { get; set; }
+
         public DateTime CreatedAt { get; set; }
         public Transaction Transaction { get; set; }
     }
--- src/ExpensesTracker.API/Models/Transaction.cs
+++ src/ExpensesTracker.API/Models/Transaction.cs
@@ -1,18 +1,38 @@
-﻿namespace ExpensesTracker.API.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensesTracker.API.Data.Models
 {
     public class Transaction
     {
         public Guid Id { get; set; }
+
+        [Required]
         public Guid UserId { get; set; }
+
+        [Required]
         public Guid CategoryId { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "Transaction name cannot exceed 100 characters.")]
         public string Name { get; set; }
-        public string Description { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string? Description { get; set; }
+
+        [Range(1, 1000000,
+        ErrorMessage = "Amount must be greater than zero.")]
         public decimal Amount { get; set; }
+
+        [StringLength(3, MinimumLength = 3,
+        ErrorMessage = "Currency code must be a valid ISO 4217 code.")]
         public string? CurrencyCode { get; set; }
+
         public bool IsIncome { get; set; }
+
+        [Required]
         public DateTime IssueDate { get; set; }
-        public DateTime CreatedAt { get; set; }
+
+        public DateTime CreatedAt { get; set; } 
 
         public Category Category { get; set; }
         public Image? Receipt { get; set; }
