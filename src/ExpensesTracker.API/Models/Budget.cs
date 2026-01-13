cb807ab5456549426fd308f655e731f0b756c52f
Aram Al-Taki <aramtaki480@gmail.com>
2026-01-13 12:57:48 +0300

    Adding data annotations for Category.cs and Budget.cs

--- src/ExpensesTracker.API/Models/Budget.cs
+++ src/ExpensesTracker.API/Models/Budget.cs
@@ -1,15 +1,31 @@
-﻿namespace ExpensesTracker.API.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensesTracker.API.Data.Models
 {
     public class Budget
     {
+        [Required(ErrorMessage = "Id is required.")]
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Category Id is required.")]
         public Guid CategoryId { get; set; }
 
+        [Required(ErrorMessage = "Amount is required.")]
+        [Range(1, 1000000, ErrorMessage = "Amount must be between 1 and 1,000,000".)]
         public decimal Amount { get; set; }
+
+        [Required]
+        [Range(1, 12, ErrorMessage = "Month must be between 1 and 12.")]
         public int Month {  get; set; }
+
+        [Required]
+        [Range(2000, 2100, ErrorMessage = "Year must be between 2000 and 2100.")]
         public int Year { get; set; }
+
+        [Required(ErrorMessage = "CreatedAt is required.")]
         public DateTime CreatedAt { get; set; }
 
+        [Required(ErrorMessage = "Category is required.")]
         public Category Category { get; set; }
     }
 }
--- src/ExpensesTracker.API/Models/Category.cs
+++ src/ExpensesTracker.API/Models/Category.cs
@@ -1,16 +1,32 @@
-﻿namespace ExpensesTracker.API.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensesTracker.API.Data.Models
 {
     public class Category
     {
+        [Required(ErrorMessage = "Id is required.")]
         public Guid Id { get; set; }
 
+        [Required(ErrorMessage = "Budget Id is required.")]
+        public Guid BudgetId { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
         public string Description { get; set; }
-        public string Type { get; set; }
+
+        [Required(ErrorMessage = "Type is required.")]
+        public bool isIncome { get; set; }
+
+        [Required(ErrorMessage = "CreatedAt is required.")]
         public DateTime CreatedAt { get; set; }
 
+        [Required(ErrorMessage = "Budget is required.")]
         public Budget Budget { get; set; }
-        public ICollection<Transaction> Transactions { get; set; }
 
+        public ICollection<Transaction> Transactions { get; set; }
     }
 }
