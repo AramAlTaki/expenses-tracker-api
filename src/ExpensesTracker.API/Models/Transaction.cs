d867acedf8a0fb13a9581860787d88271c548fc6
Aram Al-Taki <aramtaki480@gmail.com>
2026-01-13 13:35:39 +0300

    fixing validations

--- src/ExpensesTracker.API/Models/Transaction.cs
+++ src/ExpensesTracker.API/Models/Transaction.cs
@@ -4,36 +4,40 @@ namespace ExpensesTracker.API.Data.Models
 {
     public class Transaction
     {
+        [Required(ErrorMessage = "Id is required.")]
         public Guid Id { get; set; }
 
-        [Required]
-        public Guid UserId { get; set; }
+        //[Required(ErrorMessage = "User Id is required.")]
+        //public Guid UserId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Category Id is required.")]
         public Guid CategoryId { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "Transaction name cannot exceed 100 characters.")]
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string Name { get; set; }
 
-        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(500, ErrorMessage = "Description cannot be longer than 500 characters.")]
         public string? Description { get; set; }
 
-        [Range(1, 1000000,
-        ErrorMessage = "Amount must be greater than zero.")]
+        [Required(ErrorMessage = "Amount is required.")]
+        [Range(0.01, 1000000, ErrorMessage = "Amount must be greater than zero.")]
         public decimal Amount { get; set; }
 
-        [StringLength(3, MinimumLength = 3,
-        ErrorMessage = "Currency code must be a valid ISO 4217 code.")]
+        [StringLength(3, MinimumLength = 3, ErrorMessage = "Currency code must be a valid ISO 4217 code.")]
         public string? CurrencyCode { get; set; }
 
+        [Required(ErrorMessage = "isIncome is required.")]
         public bool IsIncome { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Issue Date is required.")]
         public DateTime IssueDate { get; set; }
 
-        public DateTime CreatedAt { get; set; } 
+        [Required(ErrorMessage = "CreatedAt is required.")]
+        public DateTime CreatedAt { get; set; }
 
+        [Required(ErrorMessage = "Category is required.")]
         public Category Category { get; set; }
         public Image? Receipt { get; set; }
     }
