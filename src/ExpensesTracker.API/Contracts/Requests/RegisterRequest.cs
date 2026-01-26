92cdb12a7ebc3876a0fd9abe0f1a107f8fe14a69
Aram Al-Taki <aramtaki480@gmail.com>
2026-01-26 20:25:29 +0300

    Added some fluent validations for requests DTOs

--- src/ExpensesTracker.API/Contracts/Requests/RegisterRequest.cs
+++ src/ExpensesTracker.API/Contracts/Requests/RegisterRequest.cs
@@ -4,19 +4,9 @@ namespace ExpensesTracker.API.Contracts.Requests
 {
     public class RegisterRequest
     {
-
-        [Required]
         public string Username { get; set; }
-
-        [Required]
-        [EmailAddress]
         public string Email { get; set; }
-
-        [Required]
         public string Password { get; set; }
-
-        [Required]
-        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
